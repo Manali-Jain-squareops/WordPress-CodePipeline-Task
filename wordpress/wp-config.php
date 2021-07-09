f25f50b087e961623f9aac233fd5df57790a431f
Manali-Jain-squareops <manalij62@gmail.com>
2021-07-09 21:48:28 +0530

    cp

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -56,8 +56,8 @@ define( 'AUTH_SALT',        'put your unique phrase here' );
 define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
-define('WP_HOME','http://manali.squareops.xyz/');
-define('WP_SITEURL','http://manali.squareops.xyz/');
+define('WP_HOME','https://manali.squareops.xyz/');
+define('WP_SITEURL','https://manali.squareops.xyz/');
 /**#@-*/
 
 /**
